344ef1a5da0c826bbddf12fc4112ce1323cdfbc8
kyaw swar lynn <kyawswarlynn14@gmail.com>
2023-09-24 12:06:31 +0630

    add scroll button

--- app/page.js
+++ app/page.js
@@ -1,15 +1,34 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
 import Main from "../components/Main";
 import Service from "../components/Service";
 import Portfolio from "../components/Portfolio";
 import Footer from "../components/Footer";
 import Blog from "@/components/Blog";
+import {IoArrowUp} from "react-icons/io5"
 
 export default function Home() {
   const [darkMode, setDarkMode] = useState(false)
+  const [ show, setShow ] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      if(window.scrollY > 100) {
+        setShow(true);
+      } else {
+        setShow(false);
+      }
+    })
+  }, [])
+
+  const handleScrollTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    })
+  }
 
   return (
     <div className={`${darkMode ? "dark bg-gray-800" : "bg-gradient-to-r from-[#ff69b0] to-[#ffb5a8]"} text-white m-0`}>
@@ -29,6 +48,8 @@ export default function Home() {
         </div>
 
         <Footer />
+
+        {show && <button className='fixed bottom-10 right-10  p-3 rounded dark:bg-[#ff69b0]  bg-[#1f3150] bg-opacity-90 shadow' onClick={handleScrollTop}><IoArrowUp className='text-xl text-white'/></button>}
     </div>
   )
 }
