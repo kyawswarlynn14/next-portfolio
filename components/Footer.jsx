fe2709b5d567fb54a7af5745ad55be4c0ce50150
kyaw swar lynn <kyawswarlynn14@gmail.com>
2023-10-12 13:07:01 +0630

    fix footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { AiFillPhone, AiFillMail, AiOutlineCopyrightCircle } from "react-icons/ai";
-import { GoLocation } from "react-icons/go";
 import ksl from '../public/images/ksl.jpg'
 import Image from "next/image";
+import { FaTelegram } from "react-icons/fa";
 
 function Footer() {
   const year = new Date().getFullYear();
@@ -19,11 +19,15 @@ function Footer() {
           <h3 className="text-lg mb-2 font-semibold text-center tracking-wider">Contact Me</h3>
 
           <p className="text-sm md:text-base">
-            <AiFillPhone className="inline-flex mr-2" /> +959266654144, +959767600246
+            <AiFillPhone className="inline-flex mr-2" /> +959266654144
           </p>
 
           <p className="text-sm md:text-base">
             <AiFillMail className="inline-flex mr-2" /> kyawswarlynn14@gmail.com
+          </p>
+
+          <p className="text-sm md:text-base">
+            <FaTelegram href="https://t.me/kyawswarlynn14" className="inline-flex mr-2" /> kyawswarlynn14
           </p>
         </div>
 
@@ -32,7 +36,7 @@ function Footer() {
           <div className="flex gap-4 ">
             <a href="#home" className=" text-gray-100 hover:underline underline-offset-2">Home</a>
             <span>|</span>
-            <a href="#service" className=" text-gray-100 hover:underline underline-offset-2">Service</a>
+            <a href="#services" className=" text-gray-100 hover:underline underline-offset-2">Service</a>
 
             <span>|</span>
 
