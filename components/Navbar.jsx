4997a48110cad24e67e1cd9420016be4e139e94b
kyaw swar lynn <kyawswarlynn14@gmail.com>
2023-11-17 14:21:52 +0630

    fix mobile nav

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -12,7 +12,7 @@ export default function Navbar({token }) {
   const pathname = usePathname();
 
   return (
-    <nav className={` md:text-lg bg-clip-padding backdrop-filter backdrop-blur-xl bg-opacity-[0.2] sticky top-0 z-20 rounded-b-xl border-b-2 border-white py-1  px-2 md:px-0 flex-col`}>
+    <nav className={` md:text-lg bg-clip-padding backdrop-filter backdrop-blur-xl bg-opacity-[0.2] sticky top-0 z-20 rounded-b-xl border-b-2 border-white py-3 md:py-1  px-2 md:px-0 flex-col`}>
       <div className="md:w-[95%] lg:w-[80%] mx-auto flex justify-between items-center">
         <Link href={'/'} className=" font-bold tracking-wider font-mono">DevelopedByKSL</Link>
             
