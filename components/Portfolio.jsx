76fcbebf2a0f4da340c2e7ff7710860e8478df6c
kyaw swar lynn <kyawswarlynn14@gmail.com>
2023-11-17 14:49:59 +0630

    edit portfolio card

--- components/Portfolio.jsx
+++ components/Portfolio.jsx
@@ -1,59 +1,70 @@
 import Image from "next/image";
 import { Tilt } from "react-tilt";
-import { motion } from "framer-motion";
-import { fadeIn, textVariant } from "../utils/motion";
 import { CgWebsite } from "react-icons/cg";
 import { portfolioData } from "@/config/data";
 
 const ProjectCard = ({index, name, description, image, source_code_link, demo_link}) => {
+  const Card = () => {
+    return (
+      <>
+      <div className='relative w-full h-[250px]'>
+        <Image
+          width={500}
+          height={500}
+          src={image}
+          alt='project_image'
+          className='w-full h-full object-cover rounded-2xl'
+        />
+
+        <div className='absolute inset-0 flex gap-4 justify-end m-3'>
+          <div
+            onClick={() => window.open(demo_link, "_blank")}
+            className='black-gradient w-10 h-10 rounded-xl flex justify-center items-center cursor-pointer bg-slate-700'
+          >
+            <CgWebsite />
+          </div>
+
+          <div
+            onClick={() => window.open(source_code_link, "_blank")}
+            className='black-gradient w-10 h-10 rounded-xl flex justify-center items-center cursor-pointer bg-slate-700'
+          >
+            <Image 
+              width={100}
+              height={100}
+              src='/images/github.png'
+              alt='source code'
+              className='w-1/2 h-1/2 object-contain'
+            />
+          </div>
+        </div>
+      </div>
+
+      <div className='mt-3'>
+        <h3 className='text-white font-bold text-[18px]'>{name}</h3>
+        <p className=' text-[#00FF00] text-[14px]'># {description}</p>
+      </div>
+      </>
+    )
+  }
   return (
-    <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
+    <>
       <Tilt
         options={{
           max: 45,
           scale: 1,
           speed: 450,
         }}
-        className='p-5 rounded-2xl w-full shadow-[0px_2px_10px_5px_rgba(0,0,0,0.2)] dark:shadow-slate-700'
+        className='p-5 rounded-2xl w-full shadow-[0px_2px_10px_5px_rgba(0,0,0,0.2)] dark:shadow-slate-700 hidden md:block'
       >
-        <div className='relative w-full h-[250px]'>
-          <Image
-            width={500}
-            height={500}
-            src={image}
-            alt='project_image'
-            className='w-full h-full object-cover rounded-2xl'
-          />
-
-          <div className='absolute inset-0 flex gap-4 justify-end m-3'>
-            <div
-              onClick={() => window.open(demo_link, "_blank")}
-              className='black-gradient w-10 h-10 rounded-xl flex justify-center items-center cursor-pointer bg-slate-700'
-            >
-              <CgWebsite />
-            </div>
-
-            <div
-              onClick={() => window.open(source_code_link, "_blank")}
-              className='black-gradient w-10 h-10 rounded-xl flex justify-center items-center cursor-pointer bg-slate-700'
-            >
-              <Image 
-                width={100}
-                height={100}
-                src='/images/github.png'
-                alt='source code'
-                className='w-1/2 h-1/2 object-contain'
-              />
-            </div>
-          </div>
-        </div>
-
-        <div className='mt-3'>
-          <h3 className='text-white font-bold text-[18px]'>{name}</h3>
-          <p className=' text-[#00FF00] text-[14px]'># {description}</p>
-        </div>
+        <Card />
       </Tilt>
-    </motion.div>
+
+      <div
+        className='p-5 rounded-2xl w-full shadow-[0px_2px_10px_5px_rgba(0,0,0,0.2)] dark:shadow-slate-700 md:hidden'
+      >
+        <Card />
+      </div>
+    </>
   );
 };
 
--- utils/Heading.jsx
+++ utils/Heading.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import ksl from '../public/images/ksl.jpg'
 
 const Heading = ({title, description, keywords}) => {
   return (
@@ -7,7 +8,7 @@ const Heading = ({title, description, keywords}) => {
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <meta name='description' content={description} />
         <meta name='keywords' content={keywords} />
-        <meta property="og:image" content="https://kyawswarlynn.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fksl.ae30a3ea.jpg&w=640&q=75" />
+        <meta property="og:image" content="https://scontent.fmdl4-2.fna.fbcdn.net/v/t1.6435-9/89940419_511555066427184_5823615473123590144_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=7a1959&_nc_eui2=AeH2oolw_kiv15gvKQ0sbBLziwOUoYqYAzKLA5ShipgDMnzOErLSoASro5Uu0c_0OoATOl9k2W3HGLN5nHRkmlib&_nc_ohc=HB17LF8FgKEAX_kC0pK&_nc_ht=scontent.fmdl4-2.fna&oh=00_AfCOgBKkNoj5Xte73IJKwH4BBgKGr6X0c8Hm_1P5svrn7Q&oe=657E8AA6" />
         <meta property="og:type" content="website" />
     </>
   )
