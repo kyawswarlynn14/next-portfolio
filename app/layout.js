e142485d9b1ff5a6e2f337636b05efc30954c16b
kyaw swar lynn <kyawswarlynn14@gmail.com>
2023-11-17 14:07:33 +0630

    change default theme

--- app/layout.js
+++ app/layout.js
@@ -17,7 +17,7 @@ export default function RootLayout({children}) {
   return (
     <html lang="en" suppressHydrationWarning={true}>
       <body className={`${ubuntu.variable} main-scrollbar min-h-screen bg-no-repeat bg-gradient-to-r from-[#ff69b0] to-[#ffb5a8] dark:bg-gradient-to-r dark:from-[#0b131c] dark:to-[#1b283d] duration-100 text-white`}>
-          <ThemeProvider attribute='class' defaultTheme='system' enableSystem>
+          <ThemeProvider attribute='class' defaultTheme='dark' enableSystem>
             <Navbar token={Cookies.get("token")} />
             <div className="w-[95%] md:w-[80%] mx-auto mb-8">
               {children}
